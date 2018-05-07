1da3908d421450b82c6da1d89ee2db74eaffc51b
dpdurga <karnamdpdurga@gmail.com>
2018-05-07 13:15:31 +0530

    changed update_equation

--- Code/Vector/createEquation.js
+++ Code/Vector/createEquation.js
@@ -4,8 +4,8 @@
 createVector.prototype.create_equation = function(){
   var symbol = this.symbol;
   this.div = d3.select('body').append('div');
-  this.div.append('text').html('\\( ' +symbol+ '_x = ' +symbol+ '_r*cos( ' +symbol+ '_\\theta ) \\)' + '<br>');
-  this.div.append('text').html('\\( ' +symbol+ '_y = ' +symbol+ '_r*sin( ' +symbol+ '_\\theta ) \\)');
+  this.xEquation = this.div.append('text').html('\\( ' +symbol+ '_x = ' +symbol+ '_r*cos( ' +symbol+ '_\\theta ) \\)' + '<br>');
+  this.yEquation = this.div.append('text').html('\\( ' +symbol+ '_y = ' +symbol+ '_r*sin( ' +symbol+ '_\\theta ) \\)');
   MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
 
@@ -23,6 +23,8 @@ createVector.prototype.update_equation = function(){
   this.div.styles({ 'top': this.cy + this.r + 10, 'left': this.cx - 0.5*parseInt(this.div.style('width')) });
   if(this.vector_mode == 'polar'){ this.div.styles({ 'display': 'none' }); }
   if(this.vector_mode == 'cartesian'){ this.div.styles({ 'display': null }); }
+  this.xEquation.html('\\( ' +symbol+ '_x = ' +this.r+ '*cos( ' +this.angle+ ' ) \\)');
+  this.yEquation.html('\\( ' +symbol+ '_y = ' +this.r+ '*sin( ' +this.angle+ ' ) \\)');
 }
 
 /***********************************************************************************/
